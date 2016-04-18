5a070342b8c6dd5ec1fd130fc525a83d58c9f8fa
Nick S <nsipplswezey@gmail.com>
2016-04-17 19:17:31 -0700

    Example#1 deployed consistently.

--- docs/_site/js/examples/nodal-hello.js
+++ docs/_site/js/examples/nodal-hello.js
@@ -1,17 +1,23 @@
 'use strict';
 
-var HELLO_COMPONENT = '\nvar HelloMessage = React.createClass({\n  render: function() {\n    return <div>Hello {this.props.name}</div>;\n  }\n});\n\nReactDOM.render(<HelloMessage name={currentUserDisplayName} />, mountNode);\n';
+var HELLO_COMPONENT = "\nvar HelloMessage = React.createClass({\n  getInitialState: function() {\n    return {username: ''};\n  },\n\n  createUser: function() {\n    var unique = cuid();\n    var url = 'http://localhost:3000/v1/users/';\n    var postData = {\n      username: 'user_' + unique,\n      email: unique + '@domain.com',\n      password: 'password',\n    };\n\n    $.post(url, postData, function (data) {\n    	var id = data.data[0].id;\n        var user = 'visitor_'+id\n    	this.setState({username: user});\n      }.bind(this));\n\n  },\n\n  componentDidMount: function() {\n    this.createUser();\n\n  },\n\n  render: function() {\n    return <div>Hello {this.state.username}!</div>;\n  }\n});\n\nReactDOM.render(<HelloMessage />, mountNode);\n";
+
+
 
 ReactDOM.render(React.createElement(ReactPlayground, { codeText: HELLO_COMPONENT }), document.getElementById('helloExample'));
 
+
+//sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\\n/g' string-nodal-hello.js | sed -e "s/.*/\"\\\n&\\\n\"/" > nodal-hello-string.js
 /*
+For now, feed these commented readable Nodal example versions to that insane sed one liner up there... Jesus. Also that one liner has to be // commented or else
 
-Feed this to whatever inserts those new lines and creates that giant hello component string...
+Then paste the string into the var EXAMPLE_COMPONENT
 
+Hah. Totally insane. Fix this for later. Or do it once, and hide it. Or something. Jesus.
 
 var HelloMessage = React.createClass({
   getInitialState: function() {
-    return {username: ""};
+    return {username: ''};
   },
 
   createUser: function() {
@@ -25,7 +31,7 @@ var HelloMessage = React.createClass({
 
     $.post(url, postData, function (data) {
     	var id = data.data[0].id;
-        var user = "visitor_"+id
+        var user = 'visitor_'+id
     	this.setState({username: user});
       }.bind(this));
 
--- docs/js/examples/hello.js
+++ docs/js/examples/hello.js
@@ -1,5 +1,5 @@
 'use strict';
 
-var HELLO_COMPONENT = '\nvar HelloMessage = React.createClass({\n  render: function() {\n    return <div>Hello {this.props.name}</div>;\n  }\n});\n\nReactDOM.render(<HelloMessage name={currentUserDisplayName} />, mountNode);\n';
+var HELLO_COMPONENT = "\nvar HelloMessage = React.createClass({\n  getInitialState: function() {\n    return {username: ''};\n  },\n\n  createUser: function() {\n    var unique = cuid();\n    var url = 'http://localhost:3000/v1/users/';\n    var postData = {\n      username: 'user_' + unique,\n      email: unique + '@domain.com',\n      password: 'password',\n    };\n\n    $.post(url, postData, function (data) {\n    	var id = data.data[0].id;\n        var user = 'visitor_'+id\n    	this.setState({username: user});\n      }.bind(this));\n\n  },\n\n  componentDidMount: function() {\n    this.createUser();\n\n  },\n\n  render: function() {\n    return <div>Hello {this.state.username}!</div>;\n  }\n});\n\nReactDOM.render(<HelloMessage />, mountNode);\n";
 
 ReactDOM.render(React.createElement(ReactPlayground, { codeText: HELLO_COMPONENT }), document.getElementById('helloExample'));
